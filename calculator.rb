bb555b698e44363d03558f13ddc3a4dd839a6fc4
Saroar <saroar9@gmail.com>
2015-05-29 09:53:32 +0300

    after add case statment and clean style

--- calculator.rb
+++ calculator.rb
@@ -4,7 +4,7 @@ end
 
 speak "Whats your 1st number ?"
 number_1st = gets.chomp
-words = number_1st.to_i
+
 
 speak "Whats your 2nd number ?"
 number_2nd = gets.chomp
@@ -12,28 +12,20 @@ number_2nd = gets.chomp
 
 
 speak "What you like to do 1. add 2. subtract 3. multiply 4. divide " 
-
 operator = gets.chomp
 
-# if operator == "1"
-#   result = number_1st.to_i + number_2nd.to_i
-#   elsif operator == "2"
-#   result = number_1st.to_i - number_2nd.to_i
-#   elsif operator == "3"
-#   result = number_1st.to_i * number_2nd.to_i
-#   else operator == "4"
-#   result = number_1st.to_i / number_2nd.to_i
-# end
-
-case 
-when operator == "1"
+case operator
+when "1"
   result = number_1st.to_i + number_2nd.to_i
-when operator == "2"
+when "2"
   result = number_1st.to_i - number_2nd.to_i
-when operator == "3"
+when "3"
   result = number_1st.to_i * number_2nd.to_i
-else operator == "4"
+when "4"
   result = number_1st.to_f / number_2nd.to_f
+else
+  puts "That's is not valid oparator"
+  exit
 end
     
 puts "You're result is #{result}"
